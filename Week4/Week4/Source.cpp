6bc45851f97d2a39a2c9a21ccd40910fd5d64aba
arjun <arjun.magotra.india@gmail.com>
2016-09-27 15:47:01 +0900

    new

--- Week4/Week4/Source.cpp
+++ Week4/Week4/Source.cpp
@@ -11,32 +11,151 @@
 // this example is fro  from   http://www.glfw.org/documentation.html
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <stdio.h>
 using namespace std;
 
 
-//class Line
-//{
-//	int start_x, start_y;
-//	int end_x, end_y;
-//
-//	void draw()
-//	{
-//
-//		//to do something from start to end points
-//	}
-//
-//
-//}
+class Line
+{
+public: // p,p,p
+	int start_x, start_y;
+	int end_x, end_y;
 
 
 
+public:
+	void initialize(const int& _start_x, const int& _start_y, const int& _end_x, const int& _end_y )
+	{
+		start_x = _start_x;
+		start_y = _start_y;
+		end_x   = _end_x;
+		end_y   = _end_y;
+
+	}
+
+
+
+	void drawLine(const int& i0, const int& j0, const int& i1, const int& j1, const float& red, const float& green, const float& blue)
+	{
+		for (int i = i0; i <= i1; i++)
+		{
+			const int j = (j1 - j0)*(i - i0) / (i1 - i0) + j0;
+			//int x = i+1, y= j+1;
+
+			drawonepixel(i, j, red, green, blue);
+			//drawonepixel(x, y, red, green, blue);
+		}
+	}
+
+
+
+	void draw()
+	{
+
+		//to do something from start to end points
+		drawLine(start_x, start_y, end_x, end_y, 1.0f, 0.0f, 0.0f);
+	}
+
+
+
+	void drawonepixel(const int& i, const int& j, const float& red, const float& green, const float& blue)
+	{
+
+
+
+		pixels[(i + width *j) * 3 + 0] = red;
+		pixels[(i + width *j) * 3 + 1] = green;
+		pixels[(i + width *j) * 3 + 2] = blue;
+
+	}
+
+
+
+
+};
+
+
+
+class Box
+{
+public: 
+
+
+	 int center_x, center_y;
+	 int x_edge;
+	int y_edge;
+
+
+	void intilize(const int& _c_x, const int& _c_y, const int& _e_x, const int& _e_y)
+	{
+
+		center_x = _c_x;
+        center_y = _c_x;
+        x_edge   = _e_x;
+        y_edge   = _e_y;
+
+	}
+
+	
+
+
+
+
+	void drawLine(const int& i0, const int& j0, const int& i1, const int& j1, const float& red, const float& green, const float& blue)
+	{
+		for (int i = i0; i <= i1; i++)
+		{
+			const int j = (j1 - j0)*(i - i0) / (i1 - i0) + j0;
+			//int x = i+1, y= j+1;
+
+			drawonepixel(i, j, red, green, blue);
+			//drawonepixel(x, y, red, green, blue);
+		}
+	}
+
+
+	void draw()
+	{
+		//draw box by drawing four lines.
+		drawLine(center_x - x_edge / 2, center_y - y_edge / 2, center_x + x_edge / 2, center_y + y_edge / 2, 1.0f, 0.0f, 0.0f);
+	}
+
+
+
+
+
+	void drawonepixel(const int& i, const int& j, const float& red, const float& green, const float& blue)
+	{
+
+
+
+		pixels[(i + width *j) * 3 + 0] = red;
+		pixels[(i + width *j) * 3 + 1] = green;
+		pixels[(i + width *j) * 3 + 2] = blue;
+
+	}
+
+};
+
+
+void initializelines()
+{
+	const int  num_lines = 10;
+	Line *my_Lines = new Line[num_lines];
+
+
+	for (int i = 0; i < num_lines; i++)
+	{
+   
+
+
+	}
+
+
+}
 
 
 //glDrawPixels(window_width, window_height, GL_RGB, GL_FLOAT, pixels);
-
-
-
-
 const int width = 640;
 const int height = 480;
 
@@ -73,98 +192,96 @@ void drawonepixel(const int& i, const int& j, const float& red, const float& gre
 //	pixels[(i + width* j) * 3 + 1] = green;
 //	pixels[(i + width* j) * 3 + 2] = blue;
 //}
+
+//void drawLine(const int& i0, const int& j0, const int& i1, const int& j1, const float& red, const float& green, const float& blue)
+//{
+//	for (int i = i0; i <= i1; i++)
+//	{
+//		const int j = (j1 - j0)*(i - i0) / (i1 - i0) + j0;
+//		//int x = i+1, y= j+1;
 //
-void drawLine(const int& i0, const int& j0, const int& i1, const int& j1, const float& red, const float& green, const float& blue)
-{
-	for (int i = i0; i <= i1; i++)
-	{
-		const int j = (j1 - j0)*(i - i0) / (i1 - i0) + j0;
-		//int x = i+1, y= j+1;
-
-		drawonepixel(i, j, red, green, blue);
-		//drawonepixel(x, y, red, green, blue);
-	}
-}
+//		drawonepixel(i, j, red, green, blue);
+//		//drawonepixel(x, y, red, green, blue);
+//	}
+//}
 
 
-void drawrectangle(const float& red, const float& green, const float& blue)
-{
-
-	for (int i = 230; i <= 300; i++)
-	{
-		int j = 460, x = 360;
-		drawonepixel(i, j, red, green, blue);
-		drawonepixel(i, x, red, green, blue);
-	}
-
-
-	for (int j = 360; j <= 460; j++)
-	{
-		int i = 230, y = 300;
-		drawonepixel(i, j, red, green, blue);
-		drawonepixel(y, j, red, green, blue);
-	}
-
-
-
-
-
-	for (int i = 330; i <= 390; i++)
-	{
-		int j = 460, x = 360;
-
-		for (int j = 360; j <= 460; j++)
-		{
-			drawonepixel(i, j, red, green, blue);
-			//drawonepixel(i, x, red, green, blue);
-		}
-	}
-
-
-
-}
-
-
-void drawtriangle(const float& red, const float& green, const float& blue)
-{
-	const int i0 = 10, i1 = 100;
-	const int j0 = 150, j1 = 200;
-	drawLine(i0, j0, i1, j1, 1.0f, 0.0f, 1.0f);
-
-	drawLine(50, 150, i1, j1, 1.0f, 0.0f, 1.0f);
-
-	drawLine(10, 150, 50, 150, 1.0f, 0.0f, 1.0f);
-}
-
-void drawpentagon(const float& red, const float& green, const float& blue)
-{
-
-	drawLine(150, 150, 200, 150, 1.0f, 0.0f, 1.0f);
-
-	drawLine(130, 180, 150, 150, 1.0f, 0.0f, 1.0f);
-
-	drawLine(200, 150, 220, 180, 1.0f, 0.0f, 1.0f);
+//void drawrectangle(const float& red, const float& green, const float& blue)
+//{
+//
+//	for (int i = 230; i <= 300; i++)
+//	{
+//		int j = 460, x = 360;
+//		drawonepixel(i, j, red, green, blue);
+//		drawonepixel(i, x, red, green, blue);
+//	}
+//
+//
+//	for (int j = 360; j <= 460; j++)
+//	{
+//		int i = 230, y = 300;
+//		drawonepixel(i, j, red, green, blue);
+//		drawonepixel(y, j, red, green, blue);
+//	}
+//
+//
+//
+//
+//
+//	for (int i = 330; i <= 390; i++)
+//	{
+//		int j = 460, x = 360;
+//
+//		for (int j = 360; j <= 460; j++)
+//		{
+//			drawonepixel(i, j, red, green, blue);
+//			//drawonepixel(i, x, red, green, blue);
+//		}
+//	}
+//
+//
+//
+//}
 
 
-	drawLine(130, 180, 180, 230, 1.0f, 0.0f, 1.0f);
+//void drawtriangle(const float& red, const float& green, const float& blue)
+//{
+//	const int i0 = 10, i1 = 100;
+//	const int j0 = 150, j1 = 200;
+//	drawLine(i0, j0, i1, j1, 1.0f, 0.0f, 1.0f);
+//
+//	drawLine(50, 150, i1, j1, 1.0f, 0.0f, 1.0f);
+//
+//	drawLine(10, 150, 50, 150, 1.0f, 0.0f, 1.0f);
+//}
 
-	drawLine(180, 230, 220, 180, 1.0f, 0.0f, 1.0f);
+//void drawpentagon(const float& red, const float& green, const float& blue)
+//{
+//
+//	drawLine(150, 150, 200, 150, 1.0f, 0.0f, 1.0f);
+//
+//	drawLine(130, 180, 150, 150, 1.0f, 0.0f, 1.0f);
+//
+//	drawLine(200, 150, 220, 180, 1.0f, 0.0f, 1.0f);
+//
+//
+//	drawLine(130, 180, 180, 230, 1.0f, 0.0f, 1.0f);
+//
+//	drawLine(180, 230, 220, 180, 1.0f, 0.0f, 1.0f);
+//
+//
+//
+//}
 
-
-
-}
-
-
-void drawcircle(const float& red, const float& green, const float& blue)
-{
-
-
-}
+//
+//void drawcircle(const float& red, const float& green, const float& blue)
+//{
+//
+//
+//}
 
 void draw()
 {
-
-
 	//white background(1.0,1.0,1.0)
 	//let assign all pixels red color
 
@@ -197,7 +314,7 @@ void draw()
 	//TODO: anti-aliasing
 	const int i0 = 30, i1 = 200;
 	const int j0 = 400, j1 = 480;
-	drawLine(i0, j0, i1, j1, 1.0f, 0.0f, 0.0f);
+	/*drawLine(i0, j0, i1, j1, 1.0f, 0.0f, 0.0f);
 
 
 
@@ -206,7 +323,7 @@ void draw()
 
 	drawtriangle(1.0f, 0.0f, 1.0f);
 
-	drawpentagon(1.0f, 0.0f, 1.0f);
+	drawpentagon(1.0f, 0.0f, 1.0f);*/
 
 }
 
